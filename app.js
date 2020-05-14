9fb52efa545b9261a0b9ba1501ebecb7ad062c36
Anurodh Kumar <anu.ckp.13@gmail.com>
2020-05-15 05:28:01 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express')
 const app = express()
+const studentRoutes = require('./api/routes/student')
+const facultyRoutes = require('./api/routes/faculty')
+
+
+app.use('/student',studentRoutes)
+app.use('/faculty',facultyRoutes)
 
 app.use((req,res,next)=>{
     res.status(200).json({
