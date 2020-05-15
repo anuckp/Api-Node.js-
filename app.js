476968d2db129a921e099fc553ae8b39dbfc4f8e
Anurodh Kumar <anu.ckp.13@gmail.com>
2020-05-15 10:48:56 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -2,7 +2,19 @@ const express = require('express')
 const app = express()
 const studentRoutes = require('./api/routes/student')
 const facultyRoutes = require('./api/routes/faculty')
+const mongoose = require('mongoose')
 
+
+
+mongoose.connect('mongodb+srv://Anurodh13:9795586131@mongodb-v2jdz.mongodb.net/test?retryWrites=true&w=majority')
+
+mongoose.connection.on('error',err=>{
+    console.log('connection failed');
+})
+
+mongoose.connection.on('connected',connected=>{
+    console.log('connected with database...');
+})
 
 app.use('/student',studentRoutes)
 app.use('/faculty',facultyRoutes)
