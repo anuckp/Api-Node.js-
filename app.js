c5337f29512e6289c9537a55885acb4ed4018674
Anurodh Kumar <anu.ckp.13@gmail.com>
2020-05-15 06:35:08 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ app.use('/student',studentRoutes)
 app.use('/faculty',facultyRoutes)
 
 app.use((req,res,next)=>{
+    res.status(404).json({
+        error:"bad request"
+    })
+})
+
+
+
+app.use((req,res,next)=>{
     res.status(200).json({
         message:"app is running lllllll"
     })
